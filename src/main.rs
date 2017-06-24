6bb04848860cdedb1994388eca2038d1b80f0abd
michele <michele.damico@gmail.com>
2017-06-24 16:38:00 +0200

    Iterazione 4 => Mutex => Ok

--- src/main.rs
+++ src/main.rs
@@ -2,12 +2,13 @@ use std::thread::spawn;
 
 fn main() {
     static N: usize = 10000;
-    let r_val = std::sync::Arc::new(0);
+    let r_val = std::sync::Arc::new(std::sync::Mutex::new(0));
     let mut handlers: Vec<_> = Default::default();
 
     for _ in 0..N {
         let r_val = r_val.clone();
         handlers.push(spawn(move || {
+            let mut r_val = r_val.lock().unwrap();
             *r_val += 1;
         }
         ));
@@ -15,6 +16,7 @@ fn main() {
     for h in handlers {
         h.join().unwrap();
     }
-    print!("Sum = {}", r_val);
-    assert_eq!(N, *r_val);
+    let result = r_val.lock().unwrap().clone();
+    print!("Sum = {}", result);
+    assert_eq!(N, result);
 }
